78948f3fda86889cb915dc41548a3f2b7ab7e4ec
Josh Buker <accounts+github@joshbuker.com>
2021-08-29 17:06:03 -0700

    Add manual receiver code, will need...all the things

---
+++ main.py
@@ -0,0 +1,181 @@
+# The following extensions are required:
+# * MaqueenPlus
+# This program is not block-code compatible.
+# ---------------
+# Hardware Values
+# ---------------
+
+RADIO_GROUP = 90
+RADIO_DEADZONE = 20
+RADIO_MAX = 1024
+
+LEFT_MOTOR = Motors.M1
+RIGHT_MOTOR = Motors.M2
+
+FORWARD = Dir.CW
+REVERSE = Dir.CCW
+
+MAX_SPEED = 80
+TURN_SPEED_RATIO = 1/2
+
+# -----------
+# Code Begins
+# -----------
+
+straight_channel = 0
+turn_channel = 0
+left_motor_speed = 0
+right_motor_speed = 0
+
+def constrain(val, min_val, max_val):
+    return min(max_val, max(min_val, val))
+
+def translate(value, left_min, left_max, right_min, right_max):
+    left_span = left_max - left_min
+    right_span = right_max - right_min
+    # Convert the left range into a 0-1 range float
+    value_scaled = (value - left_min) / left_span
+    # Convert the 0-1 range into a value in the right range
+    return right_min + (value_scaled * right_span)
+
+def on_received_value(name, value):
+    global straight_channel
+    global turn_channel
+    if name == 'straight':
+        straight_channel = value
+    elif name == 'turn':
+        turn_channel = value
+
+def radio_to_speed(radio_value):
+    speed = translate(radio_value, -RADIO_MAX, RADIO_MAX, -MAX_SPEED, MAX_SPEED)
+    speed = constrain(speed, -MAX_SPEED, MAX_SPEED)
+    return speed
+
+# Lazy implementation of mixing
+def calculate_speeds_from_radio():
+    global left_motor_speed
+    global right_motor_speed
+    # Reset to 0
+    left_motor_speed = 0
+    right_motor_speed = 0
+    # Forward/Backward
+    if abs(straight_channel) > RADIO_DEADZONE:
+        left_motor_speed = radio_to_speed(straight_channel)
+        right_motor_speed = radio_to_speed(straight_channel)
+    # Left/Right
+    if abs(turn_channel) > RADIO_DEADZONE:
+        left_motor_speed += radio_to_speed(turn_channel * TURN_SPEED_RATIO)
+        right_motor_speed -= radio_to_speed(turn_channel * TURN_SPEED_RATIO)
+    # Constrain motors to max speed when mixing
+    left_motor_speed = constrain(left_motor_speed, -MAX_SPEED, MAX_SPEED)
+    right_motor_speed = constrain(right_motor_speed, -MAX_SPEED, MAX_SPEED)
+
+def set_motor_speeds():
+    if left_motor_speed > 0:
+        DFRobotMaqueenPlus.motot_run(LEFT_MOTOR, FORWARD, left_motor_speed)
+    elif left_motor_speed < 0:
+        DFRobotMaqueenPlus.motot_run(LEFT_MOTOR, REVERSE, abs(left_motor_speed))
+    else:
+        DFRobotMaqueenPlus.motot_stop(LEFT_MOTOR)
+
+    if right_motor_speed > 0:
+        DFRobotMaqueenPlus.motot_run(RIGHT_MOTOR, FORWARD, right_motor_speed)
+    elif right_motor_speed < 0:
+        DFRobotMaqueenPlus.motot_run(RIGHT_MOTOR, REVERSE, abs(right_motor_speed))
+    else:
+        DFRobotMaqueenPlus.motot_stop(RIGHT_MOTOR)
+
+def direction_arrow():
+    # Mirror channels so that it points in the direction it's moving
+    straight = -straight_channel
+    turn = -turn_channel
+    if straight > RADIO_DEADZONE:
+        if turn > RADIO_DEADZONE:
+            basic.show_leds("""
+                            . # # # #
+                            . . . # #
+                            . . # . #
+                            . # . . #
+                            # . . . .
+                            """)
+        elif turn < -RADIO_DEADZONE:
+            basic.show_leds("""
+                            # # # # .
+                            # # . . .
+                            # . # . .
+                            # . . # .
+                            . . . . #
+                            """)
+        else:
+            basic.show_leds("""
+                            . . # . .
+                            . # # # .
+                            # . # . #
+                            . . # . .
+                            . . # . .
+                            """)
+    elif straight < -RADIO_DEADZONE:
+        if turn > RADIO_DEADZONE:
+            basic.show_leds("""
+                            # . . . .
+                            . # . . #
+                            . . # . #
+                            . . . # #
+                            . # # # #
+                            """)
+        elif turn < -RADIO_DEADZONE:
+            basic.show_leds("""
+                            . . . . #
+                            # . . # .
+                            # . # . .
+                            # # . . .
+                            # # # # .
+                            """)
+        else:
+            basic.show_leds("""
+                            . . # . .
+                            . . # . .
+                            # . # . #
+                            . # # # .
+                            . . # . .
+                            """)
+    else:
+        if turn > RADIO_DEADZONE:
+            basic.show_leds("""
+                            . . # . .
+                            . . . # .
+                            # # # # #
+                            . . . # .
+                            . . # . .
+                            """)
+        elif turn < -RADIO_DEADZONE:
+            basic.show_leds("""
+                            . . # . .
+                            . # . . .
+                            # # # # #
+                            . # . . .
+                            . . # . .
+                            """)
+        else:
+            basic.show_leds("""
+                            . # # # .
+                            # . . . #
+                            # . . . #
+                            # . . . #
+                            . # # # .
+                            """)
+
+def setup():
+    basic.show_icon(IconNames.HEART)
+    radio.on_received_value(on_received_value)
+    radio.set_group(RADIO_GROUP)
+    DFRobotMaqueenPlus.i2c_init()
+
+def loop():
+    calculate_speeds_from_radio()
+    set_motor_speeds()
+    direction_arrow()
+    # basic.pause(25) # Keep the refresh rate sane
+
+setup()
+basic.forever(loop)
--- main.ts
+++ main.ts
@@ -1,3 +1,193 @@
-basic.forever(function () {
-	
+//  The following extensions are required:
+//  * MaqueenPlus
+//  This program is not block-code compatible.
+//  ---------------
+//  Hardware Values
+//  ---------------
+let RADIO_GROUP = 90
+let RADIO_DEADZONE = 20
+let RADIO_MAX = 1024
+let LEFT_MOTOR = Motors.M1
+let RIGHT_MOTOR = Motors.M2
+let FORWARD = Dir.CW
+let REVERSE = Dir.CCW
+let MAX_SPEED = 80
+let TURN_SPEED_RATIO = 1 / 2
+//  -----------
+//  Code Begins
+//  -----------
+let straight_channel = 0
+let turn_channel = 0
+let left_motor_speed = 0
+let right_motor_speed = 0
+function constrain(val: number, min_val: number, max_val: number): number {
+    return Math.min(max_val, Math.max(min_val, val))
+}
+
+function translate(value: number, left_min: number, left_max: number, right_min: number, right_max: number): number {
+    let left_span = left_max - left_min
+    let right_span = right_max - right_min
+    //  Convert the left range into a 0-1 range float
+    let value_scaled = (value - left_min) / left_span
+    //  Convert the 0-1 range into a value in the right range
+    return right_min + value_scaled * right_span
+}
+
+function radio_to_speed(radio_value: number): number {
+    let speed = translate(radio_value, -RADIO_MAX, RADIO_MAX, -MAX_SPEED, MAX_SPEED)
+    speed = constrain(speed, -MAX_SPEED, MAX_SPEED)
+    return speed
+}
+
+//  Lazy implementation of mixing
+function calculate_speeds_from_radio() {
+    
+    
+    //  Reset to 0
+    left_motor_speed = 0
+    right_motor_speed = 0
+    //  Forward/Backward
+    if (Math.abs(straight_channel) > RADIO_DEADZONE) {
+        left_motor_speed = radio_to_speed(straight_channel)
+        right_motor_speed = radio_to_speed(straight_channel)
+    }
+    
+    //  Left/Right
+    if (Math.abs(turn_channel) > RADIO_DEADZONE) {
+        left_motor_speed += radio_to_speed(turn_channel * TURN_SPEED_RATIO)
+        right_motor_speed -= radio_to_speed(turn_channel * TURN_SPEED_RATIO)
+    }
+    
+    //  Constrain motors to max speed when mixing
+    left_motor_speed = constrain(left_motor_speed, -MAX_SPEED, MAX_SPEED)
+    right_motor_speed = constrain(right_motor_speed, -MAX_SPEED, MAX_SPEED)
+}
+
+function set_motor_speeds() {
+    if (left_motor_speed > 0) {
+        DFRobotMaqueenPlus.mototRun(LEFT_MOTOR, FORWARD, left_motor_speed)
+    } else if (left_motor_speed < 0) {
+        DFRobotMaqueenPlus.mototRun(LEFT_MOTOR, REVERSE, Math.abs(left_motor_speed))
+    } else {
+        DFRobotMaqueenPlus.mototStop(LEFT_MOTOR)
+    }
+    
+    if (right_motor_speed > 0) {
+        DFRobotMaqueenPlus.mototRun(RIGHT_MOTOR, FORWARD, right_motor_speed)
+    } else if (right_motor_speed < 0) {
+        DFRobotMaqueenPlus.mototRun(RIGHT_MOTOR, REVERSE, Math.abs(right_motor_speed))
+    } else {
+        DFRobotMaqueenPlus.mototStop(RIGHT_MOTOR)
+    }
+    
+}
+
+function direction_arrow() {
+    //  Mirror channels so that it points in the direction it's moving
+    let straight = -straight_channel
+    let turn = -turn_channel
+    if (straight > RADIO_DEADZONE) {
+        if (turn > RADIO_DEADZONE) {
+            basic.showLeds(`
+                            . # # # #
+                            . . . # #
+                            . . # . #
+                            . # . . #
+                            # . . . .
+                            `)
+        } else if (turn < -RADIO_DEADZONE) {
+            basic.showLeds(`
+                            # # # # .
+                            # # . . .
+                            # . # . .
+                            # . . # .
+                            . . . . #
+                            `)
+        } else {
+            basic.showLeds(`
+                            . . # . .
+                            . # # # .
+                            # . # . #
+                            . . # . .
+                            . . # . .
+                            `)
+        }
+        
+    } else if (straight < -RADIO_DEADZONE) {
+        if (turn > RADIO_DEADZONE) {
+            basic.showLeds(`
+                            # . . . .
+                            . # . . #
+                            . . # . #
+                            . . . # #
+                            . # # # #
+                            `)
+        } else if (turn < -RADIO_DEADZONE) {
+            basic.showLeds(`
+                            . . . . #
+                            # . . # .
+                            # . # . .
+                            # # . . .
+                            # # # # .
+                            `)
+        } else {
+            basic.showLeds(`
+                            . . # . .
+                            . . # . .
+                            # . # . #
+                            . # # # .
+                            . . # . .
+                            `)
+        }
+        
+    } else if (turn > RADIO_DEADZONE) {
+        basic.showLeds(`
+                            . . # . .
+                            . . . # .
+                            # # # # #
+                            . . . # .
+                            . . # . .
+                            `)
+    } else if (turn < -RADIO_DEADZONE) {
+        basic.showLeds(`
+                            . . # . .
+                            . # . . .
+                            # # # # #
+                            . # . . .
+                            . . # . .
+                            `)
+    } else {
+        basic.showLeds(`
+                            . # # # .
+                            # . . . #
+                            # . . . #
+                            # . . . #
+                            . # # # .
+                            `)
+    }
+    
+}
+
+function setup() {
+    basic.showIcon(IconNames.Heart)
+    radio.onReceivedValue(function on_received_value(name: string, value: number) {
+        
+        
+        if (name == "straight") {
+            straight_channel = value
+        } else if (name == "turn") {
+            turn_channel = value
+        }
+        
+    })
+    radio.setGroup(RADIO_GROUP)
+    DFRobotMaqueenPlus.I2CInit()
+}
+
+//  basic.pause(25) # Keep the refresh rate sane
+setup()
+basic.forever(function loop() {
+    calculate_speeds_from_radio()
+    set_motor_speeds()
+    direction_arrow()
 })
