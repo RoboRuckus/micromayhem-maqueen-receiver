b45db5a3107eab6731aced6e422d3d76e8e17094
CDEguia <candelario.eguia@laverne.edu>
2021-09-01 23:07:22 -0700

    Modified main.py on branch master
    moved directional leds to funtion and added turn signal funtion

--- main.py
+++ main.py
@@ -1,61 +1,33 @@
-def TurnLeft(num: number):
-    DFRobotMaqueenPlus.clear_distance(Motors.ALL)
-    DFRobotMaqueenPlus.motot_run(Motors.M1, Dir.CCW, 55)
-    DFRobotMaqueenPlus.motot_run(Motors.M2, Dir.CW, 55)
-    while abs(parse_float(DFRobotMaqueenPlus.reade_distance(Motors1.M2))) < num or abs(parse_float(DFRobotMaqueenPlus.reade_distance(Motors1.M1))) < num:
-        pass
 
 def on_received_string(receivedString):
-    if receivedString == "FA":
-        DFRobotMaqueenPlus.set_rgb_light(RGBLight.RGBL, Color.GREEN)
-        DFRobotMaqueenPlus.set_rgb_light(RGBLight.RGBR, Color.GREEN)
-        basic.show_leds("""
-            . . # . .
-            . # # # .
-            # . # . #
-            . . # . .
-            . . # . .
-        """)
-        MoveForward(1.1)
-    elif receivedString == "BA":
-        DFRobotMaqueenPlus.set_rgb_light(RGBLight.RGBL, Color.YELLOW)
-        DFRobotMaqueenPlus.set_rgb_light(RGBLight.RGBR, Color.YELLOW)
-        basic.show_leds("""
-            . . # . .
-            . . # . .
-            # . # . #
-            . # # # .
-            . . # . .
-        """)
-        MoveBackward(1.1)
-    elif receivedString == "LA":
-        DFRobotMaqueenPlus.set_rgb_light(RGBLight.RGBL, Color.GREEN)
-        basic.show_leds("""
-            . . # . .
-            . # . . .
-            # . # # #
-            . # . . .
-            . . # . .
-        """)
-        TurnLeft(_90degrees)
-    elif receivedString == "RA":
-        DFRobotMaqueenPlus.set_rgb_light(RGBLight.RGBR, Color.GREEN)
-        basic.show_leds("""
-            . . # . .
-            . . . # .
-            # # # . #
-            . . . # .
-            . . # . .
-        """)
-        TurnRight(_90degrees)
-    elif receivedString == "slowForward":
-        MoveForward(0.01)
-    elif receivedString == "slowBackward":
-        MoveBackward(0.01)
-    elif receivedString == "oneDegreeLeft":
-        TurnLeft(0.01)
-    elif receivedString == "oneDegreeRight":
-        TurnRight(0.01)
+    if receivedString == "oneForward":
+        display_direction("forward")
+        MoveForward(fullmove)
+    elif receivedString == "oneBackward":
+        display_direction("backward")
+        MoveBackward(fullmove)
+    elif receivedString == "oneLeft":
+        turn_signal("left", 3)
+        display_direction("left")
+        TurnLeft(quarterTurn)
+    elif receivedString == "oneRight":
+        turn_signal("right", 3)
+        display_direction("right")
+        TurnRight(quarterTurn)
+    elif receivedString == "littleForward":
+        display_direction("forward")
+        MoveForward(smallmove)
+    elif receivedString == "littleBackward":
+        display_direction("backward")
+        MoveBackward(smallmove)
+    elif receivedString == "littleLeft":
+        display_direction("left")
+        turn_signal("left", 2)
+        TurnLeft(smallmove)
+    elif receivedString == "littleRight":
+        display_direction("right")
+        turn_signal("right", 2)
+        TurnRight(smallmove)
     else:
         pass
     DFRobotMaqueenPlus.motot_stop(Motors.ALL)
@@ -66,26 +38,84 @@ def on_received_string(receivedString):
         . # . . .
         . . . . .
     """)
-    DFRobotMaqueenPlus.set_rgb_light(RGBLight.RGBL, Color.RED)
-    DFRobotMaqueenPlus.set_rgb_light(RGBLight.RGBR, Color.RED)
+    DFRobotMaqueenPlus.set_rgb_light(RGBLight.RGBL, Color.White)
+    DFRobotMaqueenPlus.set_rgb_light(RGBLight.RGBR, Color.White)
 radio.on_received_string(on_received_string)
 
 def on_received_value(name, value):
-    if name == "F":
+    if name == "forward":
         DFRobotMaqueenPlus.motot_run(Motors.ALL, Dir.CW, Math.map(value, 550, 1023, 10, 255))
         DFRobotMaqueenPlus.set_rgb_light(RGBLight.RGBL, Color.GREEN)
         DFRobotMaqueenPlus.set_rgb_light(RGBLight.RGBR, Color.GREEN)
-        basic.show_leds("""
-            . . # . .
-            . # . # .
-            # . # . #
-            . . # . .
-            . . # . .
-        """)
-    elif name == "B":
+        display_direction("forward")
+    elif name == "backward":
         DFRobotMaqueenPlus.motot_run(Motors.ALL, Dir.CCW, Math.map(value, 1, 540, 255, 10))
         DFRobotMaqueenPlus.set_rgb_light(RGBLight.RGBL, Color.BLUE)
         DFRobotMaqueenPlus.set_rgb_light(RGBLight.RGBR, Color.BLUE)
+        display_direction("backward")
+    elif name == "left":
+        DFRobotMaqueenPlus.motot_run(Motors.M2, Dir.CW, Math.map(value, 1, 450, 255, 40))
+        DFRobotMaqueenPlus.motot_run(Motors.M1, Dir.CW, 20)
+        DFRobotMaqueenPlus.set_rgb_light(RGBLight.RGBL, Color.GREEN)
+        display_direction("left")
+    elif name == "right":
+        DFRobotMaqueenPlus.motot_run(Motors.M1, Dir.CW, Math.map(value, 550, 1023, 40, 255))
+        DFRobotMaqueenPlus.motot_run(Motors.M2, Dir.CW, 20)
+        DFRobotMaqueenPlus.set_rgb_light(RGBLight.RGBR, Color.GREEN)
+        display_direction("right")
+radio.on_received_value(on_received_value)
+
+def TurnLeft(num: number):
+    DFRobotMaqueenPlus.clear_distance(Motors.ALL)
+    DFRobotMaqueenPlus.motot_run(Motors.M1, Dir.CCW, turnSpeed)
+    DFRobotMaqueenPlus.motot_run(Motors.M2, Dir.CW, turnSpeed)
+    while abs(parse_float(DFRobotMaqueenPlus.reade_distance(Motors1.M2))) < num or abs(parse_float(DFRobotMaqueenPlus.reade_distance(Motors1.M1))) < num:
+        pass
+
+def TurnRight(num2: number):
+    DFRobotMaqueenPlus.clear_distance(Motors.ALL)
+    DFRobotMaqueenPlus.motot_run(Motors.M1, Dir.CW, turnSpeed)
+    DFRobotMaqueenPlus.motot_run(Motors.M2, Dir.CCW, turnSpeed)
+    while abs(parse_float(DFRobotMaqueenPlus.reade_distance(Motors1.M2))) < num2 or abs(parse_float(DFRobotMaqueenPlus.reade_distance(Motors1.M1))) < num2:
+        pass
+
+def MoveBackward(num3: number):
+    DFRobotMaqueenPlus.clear_distance(Motors.ALL)
+    DFRobotMaqueenPlus.motot_run(Motors.M1, Dir.CCW, basespeed)
+    DFRobotMaqueenPlus.motot_run(Motors.M2, Dir.CCW, basespeed)
+    while abs(parse_float(DFRobotMaqueenPlus.reade_distance(Motors1.M2))) < num3 or abs(parse_float(DFRobotMaqueenPlus.reade_distance(Motors1.M1))) < num3:
+        pass
+
+def MoveForward(num4: number):
+    DFRobotMaqueenPlus.clear_distance(Motors.ALL)
+    DFRobotMaqueenPlus.motot_run(Motors.M2, Dir.CW, basespeed)
+    DFRobotMaqueenPlus.motot_run(Motors.M1, Dir.CW, basespeed)
+    while abs(parse_float(DFRobotMaqueenPlus.reade_distance(Motors1.M2))) < num4 or abs(parse_float(DFRobotMaqueenPlus.reade_distance(Motors1.M1))) < num4:
+        pass
+
+def turn_signal(direction: string, blink: number):
+    lightToBlink = "1"
+    if direction == "left":
+        lightToBlink = "2"
+    
+    for i in range(blink):
+        DFRobotMaqueenPlus.set_rgb_light(lightToBlink, Color.YELLOW)
+        basic.pause(500)
+        DFRobotMaqueenPlus.set_rgb_light(lightToBlink, Color.WHITE)
+        basic.pause(500)
+
+
+
+def display_direction(direction: string):
+    if direction == "forward":
+        basic.show_leds("""
+            . . # . .
+            . # . # .
+            # . # . #
+            . . # . .
+            . . # . .
+        """)
+    elif direction == "backward":
         basic.show_leds("""
             . . # . .
             . . # . .
@@ -93,21 +123,7 @@ def on_received_value(name, value):
             . # . # .
             . . # . .
         """)
-    elif name == "L":
-        DFRobotMaqueenPlus.motot_run(Motors.M2, Dir.CW, Math.map(value, 1, 450, 255, 40))
-        DFRobotMaqueenPlus.motot_run(Motors.M1, Dir.CW, 20)
-        DFRobotMaqueenPlus.set_rgb_light(RGBLight.RGBL, Color.GREEN)
-        basic.show_leds("""
-            . . # . .
-            . # . . .
-            # . # # #
-            . # . . .
-            . . # . .
-        """)
-    elif name == "R":
-        DFRobotMaqueenPlus.motot_run(Motors.M1, Dir.CW, Math.map(value, 550, 1023, 40, 255))
-        DFRobotMaqueenPlus.motot_run(Motors.M2, Dir.CW, 20)
-        DFRobotMaqueenPlus.set_rgb_light(RGBLight.RGBR, Color.GREEN)
+    elif direction == "left":
         basic.show_leds("""
             . . # . .
             . . . # .
@@ -115,32 +131,30 @@ def on_received_value(name, value):
             . . . # .
             . . # . .
         """)
-radio.on_received_value(on_received_value)
+    else:
+        basic.show_leds("""
+            . . # . .
+            . # . . .
+            # . # # #
+            . # . . .
+            . . # . .
+        """)
 
-def TurnRight(num2: number):
-    DFRobotMaqueenPlus.clear_distance(Motors.ALL)
-    DFRobotMaqueenPlus.motot_run(Motors.M1, Dir.CW, 55)
-    DFRobotMaqueenPlus.motot_run(Motors.M2, Dir.CCW, 55)
-    while abs(parse_float(DFRobotMaqueenPlus.reade_distance(Motors1.M2))) < num2 or abs(parse_float(DFRobotMaqueenPlus.reade_distance(Motors1.M1))) < num2:
-        pass
-def MoveBackward(num3: number):
-    DFRobotMaqueenPlus.clear_distance(Motors.ALL)
-    DFRobotMaqueenPlus.motot_run(Motors.M1, Dir.CCW, basespeed)
-    DFRobotMaqueenPlus.motot_run(Motors.M2, Dir.CCW, basespeed)
-    while abs(parse_float(DFRobotMaqueenPlus.reade_distance(Motors1.M2))) < num3 or abs(parse_float(DFRobotMaqueenPlus.reade_distance(Motors1.M1))) < num3:
-        pass
-def MoveForward(num4: number):
-    DFRobotMaqueenPlus.clear_distance(Motors.ALL)
-    DFRobotMaqueenPlus.motot_run(Motors.M2, Dir.CW, basespeed)
-    DFRobotMaqueenPlus.motot_run(Motors.M1, Dir.CW, basespeed)
-    while abs(parse_float(DFRobotMaqueenPlus.reade_distance(Motors1.M2))) < num4 or abs(parse_float(DFRobotMaqueenPlus.reade_distance(Motors1.M1))) < num4:
-        pass
 basespeed = 0
-_90degrees = 0
+quarterTurn = 0
+turnSpeed = 0
+fullmove = 0
+smallmove = 0
 radio.set_group(1)
-_90degrees = 0.35
+
 basespeed = 50
+turnSpeed = 50
+
+quarterTurn = 0.35
+fullmove = 0.75
+smallmove = 0.01
 
 def on_forever():
     pass
+
 basic.forever(on_forever)
